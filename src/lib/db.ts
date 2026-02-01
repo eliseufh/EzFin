e50bb1a4fbc915d511ebec1ff055a806bf321dc2
Eliseu Silva <eliseusilva.info@gmail.com>
2026-02-01 22:51:11 +0000

    feat: add datasources configuration to PrismaClient for database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,6 +5,11 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 const prismaClientSingleton = () => {
   return new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   }).$extends(withAccelerate());
 };
 
